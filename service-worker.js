3537677550e90ed6e513371586170d33a3fb3ba9
Diwash barla <oraondiwash02@gmail.com>
2026-03-15 13:19:09 +0530

    Add service worker for caching and fetch handling

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE="prompt-cache"
+
+self.addEventListener("install",e=>{
+
+e.waitUntil(
+
+caches.open(CACHE).then(cache=>{
+
+return cache.addAll([
+"/",
+"/index.html",
+"/script.js"
+])
+
+})
+
+)
+
+})
+
+self.addEventListener("fetch",e=>{
+
+e.respondWith(
+
+caches.match(e.request).then(res=>{
+
+return res || fetch(e.request)
+
+})
+
+)
+
+})
